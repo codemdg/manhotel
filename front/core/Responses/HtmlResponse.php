a6ca845b74b8dd63516bde64e8610820a8037dbf
codemdg <codemdg@gmail.com>
2024-11-07 15:34:14 +0300

    create dashboard page and prepare the default.php for admin page

--- front/core/Responses/HtmlResponse.php
+++ front/core/Responses/HtmlResponse.php
@@ -8,12 +8,13 @@ class HtmlResponse implements ResponseInterface
 {
 	public function render(string $view, array $args = []): void
 	{
+		$defaultView = "default.php";
 		if (!empty($args)) {
 			extract($args);
 		}
 		require ROOT_APP . "/Views/" . $view;
 
 		//add this content to the main template
-		require ROOT_APP . "/Views/default.php";
+		require ROOT_APP . "/Views/$defaultView";
 	}
 }
